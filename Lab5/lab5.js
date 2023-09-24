6e9507787cdb82ee5e80d1c533fe341c3be28631
Evan Liang <liange@hawaii.edu>
2023-09-24 13:08:40 -1000

    update

---
+++ Lab5/lab5.js
@@ -0,0 +1,15 @@
+//declare variable
+let age=20;
+let fav_num=10;
+let day_of_birth=11;
+let month_of_birth=8;
+
+//declare claculations
+let calc1 = age + fav_num / day_of_birth * month_of_birth;
+
+let calc2 = (age + fav_num) / day_of_birth * month_of_birth;
+
+//result of expression outputs
+document.getElementById("result1").innerHTML = calc1;
+
+document.getElementById("result2").innerHTML = calc2;
