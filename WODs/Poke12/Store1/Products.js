f49d19e21a1c9e2918790166c5a96a435c59b7d4
Evan Liang <liange@hawaii.edu>
2023-10-30 01:07:23 -1000

    update

--- WODs/Poke12/Store1/Products.js
+++ WODs/Poke12/Store1/Products.js
@@ -3,27 +3,27 @@ let quantity = [2,3,4,3,12];
 let itemData = [
     {
         brand: 'Gillette Sensor 3 Razor',
-        price: 1.23,
+        price: 40,
         quantityIndex: 0,
     },
     {
         brand: 'Barbasol Shaving Cream',
-        price: 2.64,
+        price: 75,
         quantityIndex: 1,
     },
     {
         brand: 'Nautica Cologne',
-        price: 6.17,
+        price: 35,
         quantityIndex: 2,
     },
     {
         brand: 'Rubbing Alcohol',
-        price: 0.98,
+        price: 45,
         quantityIndex: 3,
     },
     {
         brand: 'Colgate Classic Toothbrush',
-        price: 1.89,
+        price: 10,
         quantityIndex: 4,
     },
 ];
