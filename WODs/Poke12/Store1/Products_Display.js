51808872c0d981ded9bb6367e3c8cb656bd06649
Evan Liang <liange@hawaii.edu>
2023-10-30 00:37:15 -1000

    update

--- WODs/Poke12/Store1/Products_Display.js
+++ WODs/Poke12/Store1/Products_Display.js
@@ -89,14 +89,12 @@ for (let i=0; i < products.length; i++) {
 
 
 //this should be two functions or more but... it changes the item name to rotate for the images and updates
-function changeClassName(element){
+function changeClassName(element) {
     if(element.className=='item'){
-        element.className = 'item rotate';
         spins=spins+1;
-
-    }
-
-    //spins=spins+1;
+        element.className = 'item rotate';
+    } 
+    //spins=spins+1; 
     if(spins<2*hits&&hits<spins){
         //wins=true;
         over_half=true;
@@ -105,14 +103,10 @@ function changeClassName(element){
     }
     //win_span.innerHTML=wins;
     win_span.innerHTML=over_half;
-    spins_span.innerHTML=spins;
+    spins_span.innerHTML = spins; 
     hit_spin_span.innerHTML=(hits/spins).toFixed(2);
-}
-
-
-// -- Winning progress depends on hits/spins
+   // -- Winning progress depends on hits/spins
 hits_spins_ratio = hits/spins;
-let hits_spins_ratio=hits/spins;
 let progress;
 /*if ( hits_spins_ratio > 0 ) {
     progress = 'On your way!';
@@ -128,17 +122,20 @@ let progress;
 else {
     progress = 'Get going!' ;
 } */
-if(hits_spins_ratio >= 0.5 && hits<spins) {
-    progress='You win!';
-} else if(hits_spins_ratio >= 0.25) {
-progress='Almost there!';
-} else if(hits_spins_ratio>0) {
-progress='On your way!';
+if ( hits_spins_ratio >= 0.5 && hits<spins) {
+    if( hits < spins) { 
+        progress = 'You win!';
+    }
+    } else if (hits_spins_ratio>=0.25){
+        progress='Almost there!';
+    } else if (hits_spins_ratio>0) {
+        progress='On your way!';
+    }
+    else{
+        progress ='Get going!';
+    }
+    win_span.innerHTML=progress;
 }
-else {
-progress='Get going!';
-}
-win_span.innerHTML=progress;
 
 
 //this should be two functions or more but... it changes the item name by removing the rotate for the images and
