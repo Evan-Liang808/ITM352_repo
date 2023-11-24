e709a33d0f557dd0a0cd700b6925040778f09780
Evan Liang <liange@hawaii.edu>
2023-11-23 17:00:10 -1000

    update

---
+++ Lab14/l14a2.js
@@ -0,0 +1,15 @@
+const fs=require('fs');
+
+let filename= __dirname="/user_data.json";
+
+let data = fs.readFileSync(filename, 'utf-8');
+
+let user_reg_data= JSON.parse(data);
+
+console.log(user_reg_data);
+
+
+
+
+
+
